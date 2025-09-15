932f1894aa4e2076f9c4806e933b679cdb001831
Akash Wagh <akashwagh242@gmail.com>
2025-09-15 12:58:49 +0530

    Update js conditionals

--- 03_JavaScript/vid 56- js conditionals/index.js
+++ 03_JavaScript/vid 56- js conditionals/index.js
@@ -28,3 +28,19 @@ else {
     console.log("You cannot drive");
 }
 
+a=6;
+b=8;
+
+let c = a>b?(a-b):(b-a);
+console.log(c)
+
+/*  translates to:
+if (a>b){
+    let c=a-b;
+}
+else{
+    let c=b-a
+}
+    */
+
+
